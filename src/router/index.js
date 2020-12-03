3225c2baabdc33a5c2dfde1eabd970dfbc83087d
Minh Duc Nguyen <minh.nguyen@ardc.edu.au>
2020-12-04 10:12:51 +1100

    IGSN-289. Added style guide to the Editor

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import VueRouter from 'vue-router'
 import Editor from '../views/Editor.vue'
 import Dashboard from '../views/Dashboard.vue'
 import NotFound from '../views/NotFound'
+import StyleGuide from '../views/StyleGuide'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', name: 'Dashboard', component: Dashboard },
+  { path: '/style-guide', name: 'StyleGuide', component: StyleGuide },
   { path: '/edit/:schema', name: 'CreateIGSN', component: Editor },
   { path: '/edit/:schema/:prefix/:igsn', name: 'EditByIGSN', component: Editor },
   { path: '*', name: '404', component: NotFound}
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -28,84 +28,76 @@
             <router-link
                 :to="{ name: 'CreateIGSN', params: { schema: 'ardc-igsn-desc-1.0' }}"
                 class="btn btn-blue"
-            ><i class="fas fa-plus"></i> Create Resource</router-link>
+            ><i class="fas fa-plus"></i> Create Resource
+            </router-link>
           </div>
         </div>
       </div>
 
       <div v-if="result && result.totalElements === 0">
-          There are no records to be displayed! 
+        There are no records to be displayed!
       </div>
 
-      <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8" v-if="result && result.totalElements > 0">
+      <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8"
+           v-if="result && result.totalElements > 0">
         <div
-          class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200"
+            class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200"
         >
-          <table class="table-fixed min-w-full divide-y divide-gray-200">
+          <table class="table">
             <thead>
-              <tr>
-                <th
-                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                >IGSN</th>
-                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Status
-                </th>
-                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Updated
-                </th>
-                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Visibility
-                </th>
-                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Owner
-                </th>
-                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Actions</th>
-              </tr>
+            <tr>
+              <th>IGSN</th>
+              <th>Status</th>
+              <th>Updated</th>
+              <th>Visibility</th>
+              <th>Owner</th>
+              <th>Actions</th>
+            </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-500">
-              <tr v-for="record in result.content" :key="record.id">
-                <td class="px-2 py-1 border-b border-gray-200">
-                  <div class="text-sm leading-5 font-medium text-gray-900">
-                    <a
+            <tr v-for="record in result.content" :key="record.id">
+              <td>
+                <div class="text-sm leading-5 font-medium text-gray-900">
+                  <a
                       class="hover:text-blue-500"
                       target="_blank"
                       v-bind:href="getPortalUrl(record)"
-                    >{{ record.title }}</a>
-                    <p></p>
-                    <span class="text-gray-700 text-sm">{{ record.igsn.value }}</span>
-                  </div>
-                </td>
-                <td class="px-2 py-1 border-b border-gray-200 whitespace-nowrap text-sm text-center">
-                  <p>
-                    {{ record.status }}
-                  </p>
-                </td>
-                <td class="px-2 py-1 border-b border-gray-200 whitespace-no-wrap text-sm text-center">
-                  {{ record.modifiedAt | formatDate }}
-                </td>
-                <td class="px-2 py-1 border-b border-gray-200 text-center">
-                  <p class="text-sm whitespace-no-wrap">
+                  >{{ record.title }}</a>
+                  <p></p>
+                  <span class="text-gray-700 text-sm">{{ record.igsn.value }}</span>
+                </div>
+              </td>
+              <td class="text-sm text-center">
+                <p>
+                  {{ record.status }}
+                </p>
+              </td>
+              <td class="whitespace-no-wrap text-sm text-center">
+                {{ record.modifiedAt | formatDate }}
+              </td>
+              <td class="whitespace-no-wrap text-center">
+                <p class="text-sm">
                     <span v-if="record.embargoDate" class="tag tag-yellow">
                       Under Embargo
                     </span>
-                  </p>
-                  <span
-                      v-if="record.visible"
-                      class="tag tag-green"
-                  >Public</span>
-                  <span
+                </p>
+                <span
+                    v-if="record.visible"
+                    class="tag tag-green"
+                >Public</span>
+                <span
                     v-if="!record.visible && !record.embargoDate"
                     class="tag tag-red"
-                  >Private</span>
-                </td>
-                <td class="px-2 py-1 border-b border-gray-200 text-sm text-center">
-                  <span v-if="record.ownerType === 'User'">Me</span>
-                  <span v-if="record.ownerType === 'DataCenter'">{{ record.ownerID | dataCenterName(user) }}</span>
-                </td>
-                <td
-                  class="px-2 py-1 text-right border-b border-gray-200 text-sm leading-5 font-medium text-center"
-                >
-                  <router-link
+                >Private</span>
+              </td>
+              <td class="text-sm text-center">
+                <span v-if="record.ownerType === 'User'">Me</span>
+                <span v-if="record.ownerType === 'DataCenter'">{{ record.ownerID | dataCenterName(user) }}</span>
+              </td>
+              <td
+                  class="text-sm font-medium text-center"
+              >
+                <router-link
                     :to="{
                         name: 'EditByIGSN',
                         params: {
@@ -115,23 +107,24 @@
                         }
                       }"
                     class="btn btn-blue"
-                  ><i class="fas fa-edit"></i> Edit</router-link>
-                </td>
-              </tr>
+                ><i class="fas fa-edit"></i> Edit
+                </router-link>
+              </td>
+            </tr>
             </tbody>
           </table>
 
           <div
-            class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+              class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
           >
             <div class="flex-1 flex justify-between sm:hidden">
               <a
-                href="#"
-                class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                  href="#"
+                  class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >Previous</a>
               <a
-                href="#"
-                class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                  href="#"
+                  class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
               >Next</a>
             </div>
             <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between" v-if="result">
@@ -140,90 +133,90 @@
                   Page
                   <span class="font-medium">{{ result.number + 1 }}</span>
                   of
-                  <span class="font-medium">{{ result.totalPages}}</span>
+                  <span class="font-medium">{{ result.totalPages }}</span>
                 </p>
               </div>
               <div>
                 <nav class="relative z-0 inline-flex shadow-sm">
                   <!-- previous -->
                   <a
-                    @click.prevent="goToPage(result.number - 1)"
-                    href="#"
-                    class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
-                    aria-label="Previous"
+                      @click.prevent="goToPage(result.number - 1)"
+                      href="#"
+                      class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
+                      aria-label="Previous"
                   >
                     <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                       <path
-                        fill-rule="evenodd"
-                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
+                          fill-rule="evenodd"
+                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                          clip-rule="evenodd"
                       />
                     </svg>
                   </a>
 
                   <!-- first page -->
                   <a
-                    v-if="!result.first"
-                    @click.prevent="goToPage(0)"
-                    href="#"
-                    class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                      v-if="!result.first"
+                      @click.prevent="goToPage(0)"
+                      href="#"
+                      class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
                   >1</a>
 
                   <!-- dot dot -->
                   <span
-                    v-if="result.number > 2"
-                    class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700"
+                      v-if="result.number > 2"
+                      class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700"
                   >...</span>
 
                   <!-- 1 before -->
                   <a
-                    v-if="result.number - 1 > 0"
-                    @click.prevent="goToPage(result.number - 1)"
-                    href="#"
-                    class="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                      v-if="result.number - 1 > 0"
+                      @click.prevent="goToPage(result.number - 1)"
+                      href="#"
+                      class="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
                   >{{ result.number }}</a>
 
                   <!-- current page -->
                   <a
-                    @click.prevent="goToPage(result.number + 1)"
-                    href="#"
-                    class="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
-                  >{{ result.number + 1}}</a>
+                      @click.prevent="goToPage(result.number + 1)"
+                      href="#"
+                      class="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                  >{{ result.number + 1 }}</a>
 
                   <!-- 1 after -->
                   <a
-                    v-if="!(result.number + 2 >= result.totalPages)"
-                    @click.prevent="goToPage(result.number + 1)"
-                    href="#"
-                    class="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
-                  >{{ result.number +2 }}</a>
+                      v-if="!(result.number + 2 >= result.totalPages)"
+                      @click.prevent="goToPage(result.number + 1)"
+                      href="#"
+                      class="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                  >{{ result.number + 2 }}</a>
 
                   <!-- dot dot -->
                   <span
-                    v-if="result.number < result.totalPages - 2"
-                    class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700"
+                      v-if="result.number < result.totalPages - 2"
+                      class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700"
                   >...</span>
 
                   <!-- last -->
                   <a
-                    v-if="!result.last"
-                    @click.prevent="goToPage(result.totalPages-1)"
-                    href="#"
-                    class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                      v-if="!result.last"
+                      @click.prevent="goToPage(result.totalPages-1)"
+                      href="#"
+                      class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
                   >{{ result.totalPages }}</a>
 
                   <!-- next -->
                   <a
-                    @click.prevent="goToPage(result.number + 1)"
-                    href="#"
-                    class="-ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
-                    aria-label="Next"
+                      @click.prevent="goToPage(result.number + 1)"
+                      href="#"
+                      class="-ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
+                      aria-label="Next"
                   >
                     <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                       <path
-                        fill-rule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clip-rule="evenodd"
+                          fill-rule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clip-rule="evenodd"
                       />
                     </svg>
                   </a>
@@ -267,22 +260,25 @@ export default {
   },
   methods: {
     getPortalUrl(record) {
+      if (record.igsn) {
         return `${this.portalUrl}view/${record.igsn.value}`
+      }
+      return ""
     },
     getIGSNStructure(record) {
-        return {
-          prefix: record.igsn.value.split('/')[0],
-          igsn: record.igsn.value.split('/')[1]
-        }
+      return {
+        prefix: record.igsn.value.split('/')[0],
+        igsn: record.igsn.value.split('/')[1]
+      }
     },
     getRecords() {
       this.loading = true;
       this.$registryService
-        .getUserIGSNRecords(this.page, this.size, this.query)
-        .then((data) => {
-          this.result = data;
-          this.loading = false;
-        });
+          .getUserIGSNRecords(this.page, this.size, this.query)
+          .then((data) => {
+            this.result = data;
+            this.loading = false;
+          });
     },
     goToPage(num) {
       this.page = num;
